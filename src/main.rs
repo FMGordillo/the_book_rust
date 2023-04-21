4f30800d687c64acd334147b1833c524132a53f1
Facundo Martin Gordillo <me@facundogordillo.com>
2023-04-22 00:09:26 +0200

    feat: added logs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn start_game() {
+struct Log {
+    guessed_number: u32,
+}
+
+fn start_game(logs: &mut Vec<Log>) {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -22,6 +26,10 @@ fn start_game() {
         };
 
         println!("You guessed {}", guess);
+        let log = Log {
+            guessed_number: guess,
+        };
+        logs.push(log);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
@@ -32,11 +40,33 @@ fn start_game() {
             Ordering::Greater => println!("Too big"),
         }
     }
+
+    println!("Do you want to see the logs? [y/N]");
+
+    let mut answer = String::new();
+    io::stdin()
+        .read_line(&mut answer)
+        .expect("Failed to read line");
+
+    match answer.trim().to_lowercase().as_str() {
+        "y" | "yes" => {
+            println!("You've guessed for {} times", logs.len());
+            for (i, log) in logs.iter().enumerate() {
+                if logs.len() == i + 1 {
+                    println!("{}", log.guessed_number);
+                } else {
+                    print!("{} | ", log.guessed_number);
+                }
+            }
+        }
+        _ => (),
+    }
 }
 
 fn main() {
     loop {
-        start_game();
+        let mut logs: Vec<Log> = Vec::new();
+        start_game(&mut logs);
         println!("Do you want to play again? [Y/n]");
 
         let mut answer = String::new();
@@ -45,7 +75,6 @@ fn main() {
             .expect("Failed to read line");
 
         match answer.trim().to_lowercase().as_str() {
-            "y" | "yes" => start_game(),
             "n" | "no" => break,
             _ => continue,
         }
